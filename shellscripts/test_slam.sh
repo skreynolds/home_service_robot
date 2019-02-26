2bbe6ba37e967325b991fb235d1af3ae4edcc864
skreynolds <shane.k.reynolds@gmail.com>
2019-02-26 20:16:01 +0930

    Update: modified slam test shell script to include teleop package launch

--- shellscripts/test_slam.sh
+++ shellscripts/test_slam.sh
@@ -2,4 +2,8 @@
 
 gnome-terminal -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/shane/Documents/catkin_navigation_ws/src/home_service_robot/world/u_world.world" &
 
-sleep 5				
+sleep 5
+
+gnome-terminal -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
+
+sleep 5
