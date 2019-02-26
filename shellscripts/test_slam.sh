6b685376d2b8d8aa95e1275fb261f38cf74471b1
skreynolds <shane.k.reynolds@gmail.com>
2019-02-26 20:06:59 +0930

    Add: added slam testing shell script which launches the robot world and model

---
+++ shellscripts/test_slam.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+gnome-terminal -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/shane/Documents/catkin_navigation_ws/src/home_service_robot/world/u_world.world" &
+
+sleep 5				
